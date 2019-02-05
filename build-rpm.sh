905d3a101c69a46d749f27c939a79dec1ce39097
Ihor Skalskyi <igorskalskiy@gmail.com>
2019-02-05 23:05:27 +0200

    Update build-rpm.sh

--- build-rpm.sh
+++ build-rpm.sh
@@ -3,13 +3,13 @@
 #yum install -y wget epel-release
 #yum install -y rpmrebuild
 
-mkdir -v /mnt/sdb/build/rpm
-mkdir -v /mnt/sdb/build/packages
-cd /mnt/sdb/build/rpm
+#mkdir -v /mnt/sdb/build/rpm
+#mkdir -v /mnt/sdb/build/packages
+#cd /mnt/sdb/build/rpm
 
 echo -e
 #time wget -r -np -nd -A "*.rpm*" http://mirror.centos.org/centos/7/os/x86_64/Packages/ && echo OK || exit 1
-time wget -r -np -nd -A "*.rpm*" ftp://centos.colocall.net/centos/7/os/x86_64/Packages/ && echo OK || exit 1
+#time wget -r -np -nd -A "*.rpm*" ftp://centos.colocall.net/centos/7/os/x86_64/Packages/ && echo OK || exit 1
 
 for file in *
 do
