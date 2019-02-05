97fab4f592969c9296813492fa9a623b368e09a3
Ihor Skalskyi <igorskalskiy@gmail.com>
2019-02-05 12:39:44 +0200

    Update build-rpm.sh

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-mkdir -v rpm
-mkdir -v packages
-cd rpm
+#yum install -y wget epel-release
+#yum install -y rpmrebuild
+
+mkdir -v /mnt/sdb/build/rpm
+mkdir -v /mnt/sdb/build/packages
+cd /mnt/sdb/build/rpm
+
 echo -e
 #time wget -r -np -nd -A "*.rpm*" http://mirror.centos.org/centos/7/os/x86_64/Packages/ && echo OK || exit 1
 time wget -r -np -nd -A "*.rpm*" ftp://centos.colocall.net/centos/7/os/x86_64/Packages/ && echo OK || exit 1
 
 for file in *
 do
-rpmrebuild -d /mnt/vdb/packages --change-spec-preamble='sed -e "s/^Packager:.*/Packager:MySystem/" -e "s/^Vendor:.*/Vendor:MySystemLTD/"' -p -n $file
+sudo rpmrebuild -d /mnt/sdb/build/packages --change-spec-preamble='sed -e "s/^Packager:.*/Packager:MySystem/" -e "s/^Vendor:.*/Vendor:MySystemLTD/"' -p -n $file
 done
 
 #createrepo packages/x86_64/
